eef6fcd1386142ea908a7b9178abcee5bd6cf468
dhurwit <hurwit.d@husky.neu.edu>
2016-02-23 14:38:32 -0500

    add data transfer capabilities

--- readinput.py
+++ readinput.py
@@ -6,7 +6,8 @@
 import time
 import os
 import RPi.GPIO as GPIO
- 
+import csv
+
 GPIO.setmode(GPIO.BCM)
 DEBUG = 1
  
@@ -59,47 +60,27 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
  
 # 10k trim pot connected to adc #0
-potentiometer_adc = 0;
- 
-last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 5       # to keep from being jittery we'll only change
-                    # volume when the pot has moved more than 5 'counts'
- 
-while True:
-        # we'll assume that the pot didn't move
-        trim_pot_changed = False
- 
-        # read the analog pin
-        trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        # how much has it changed since the last read?
-        pot_adjust = abs(trim_pot - last_read)
- 
-        if DEBUG:
-                print "trim_pot:", trim_pot
-                print "pot_adjust:", pot_adjust
-                print "last_read", last_read
- 
-        if ( pot_adjust > tolerance ):
-               trim_pot_changed = True
- 
-        if DEBUG:
-                print "trim_pot_changed", trim_pot_changed
- 
-        if ( trim_pot_changed ):
-                set_volume = trim_pot / 10.24           # convert 10bit adc0 (0-1024) trim pot read into 0-100 volume level
-                set_volume = round(set_volume)          # round out decimal value
-                set_volume = int(set_volume)            # cast volume as integer
- 
-                print 'Volume = {volume}%' .format(volume = set_volume)
-                set_vol_cmd = 'sudo amixer cset numid=1 -- {volume}% > /dev/null' .format(volume = set_volume)
-                os.system(set_vol_cmd)  # set volume
- 
-                if DEBUG:
-                        print "set_volume", set_volume
-                        print "tri_pot_changed", set_volume
- 
-                # save the potentiometer reading for the next loop
-                last_read = trim_pot
- 
-        # hang out and do nothing for a half second
-        time.sleep(0.5)
+fsr_adc = 0;
+bit_log = []
+
+filename = 'datalog.csv'
+datalog = os.path.join(os.getcwd(), filename)
+
+def test():
+    try:
+        while True:
+            fsr_bits = readadc(fsr_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)   
+            bit_log.append(fsr_bits)
+            print fsr_bits
+            time.sleep(0.2)
+    except KeyboardInterrupt:
+        GPIO.cleanup()
+        with open(datalog, "w+") as output:
+            writer = csv.writer(output, lineterminator = '\n')
+            for val in bit_log:
+                writer.writerow([val])
+        pass 
+    
+keep_going = raw_input("Would you like to test?")
+if keep_going:
+    test()

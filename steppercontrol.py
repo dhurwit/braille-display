e8af643181e214bc176b5ff0d5da59efe1f18605
dhurwit <hurwit.d@husky.neu.edu>
2016-04-08 16:34:18 -0400

    Created controller classes

--- steppercontrol.py
+++ steppercontrol.py
@@ -1,21 +1,38 @@
-import time
 import atexit
 from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
 
+class Motor:
+    def __init__(self):
+        self.mh = Adafruit_MotorHAT(addr= 0x60)
+        self.stepper = self.mh.getStepper(200, 1)
+        
 
-def turnoffmotors():
-    mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
-    mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+    def turnoffmotors(self):
+        self.mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+        self.mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
 
-def microstep(numsteps):
-    stepper.step(numsteps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
 
-mh = Adafruit_MotorHAT(addr= 0x60)
-stepper = mh.getStepper(200, 1)
-stepper.setSpeed(30)
+    def microstep(self, numsteps, speed):
+        self.stepper.setSpeed(speed)
+        self.stepper.step(numsteps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.MICROSTEP)
 
-atexit.register(turnoffmotors)
+
+    def revmicrostep(self, numsteps, speed):
+        self.stepper.setSpeed(speed)
+        self.stepper.step(numsteps, Adafruit_MotorHAT.REVERSE, Adafruit_MotorHAT.MICROSTEP)
+
+
+    def singlestep(self, numsteps, speed):
+        self.stepper.setSpeed(speed)
+        self.stepper.step(numsteps, Adafruit_MotorHAT.FORWARD, Adafruit_MotorHAT.SINGLE)
+
+
+    def onestep(self, direction, style):
+        self.stepper.oneStep(direction, style)
+       
+
+
 
 
